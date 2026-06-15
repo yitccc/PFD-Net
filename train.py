7f705ddd8b1c2190649a565add3371db7c969acc
yitccc <100843561+yitccc@users.noreply.github.com>
2026-06-16 00:38:45 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
-    model = RTDETR(r"improve_multimodal/rtdetr/p2.yaml")  # 多模态模型
+    model = RTDETR(r"improve_multimodal/p2.yaml")  # 多模态模型
     # model.load('D:/BaiduNetdiskDownload/YOLO-multimodal/YOLO-multimodal/runs\detect/train/weights/best.pt')
     model.train(data=r"data.yaml",  # 数据集路径
                 batch=4,
